2673ce96f49d599a8b711d936d34a86df006dcda
Krushna Waghumbare <waghumbarekrushna143@gmail.com>
2024-04-04 11:21:11 +0530

    task completed done all changes

---
+++ src/components/Card.jsx
@@ -0,0 +1,188 @@
+import React, { useState } from 'react';
+
+const Card = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen1, setIsModalOpen1] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    return (
+        <>
+            <section className='bg-blue-50 h-screen w-full'>
+                <div className="mx-auto max-w-7xl">
+                    <div className='flex text-center items-center justify-between h-16 py-16'>
+                        <div className='flex justify-center items-center'>
+                            <div className='me-auto h-10 w-12 border border-gray-700 rounded-full text-white bg-gradient-to-r to-yellow-500 from-slate-100 flex justify-center items-center'>
+                                <span className='text-black font-bold text-2xl'>A</span>
+                            </div>
+                            <h1 className='font-bold text-2xl ms-4 hidden md:block'>Adam Cooper Team</h1>
+                        </div>
+                        <div className='p-2'>
+                            <button className="btn bg-slate-900 text-white rounded h-12 w-36" onClick={() => setIsModalOpen(true)}>New Project</button>
+                            {isModalOpen &&
+                                <dialog className={`modal ${isModalOpen ? 'w-full md:w-2/6' : 'hidden'} bg-white shadow-lg rounded-xl`} open>
+                                    <div className="modal-box">
+                                        <div className="modal-action p-5">
+                                            <form method="dialog">
+                                                <div className='flex justify-between'>
+                                                    <span className='text-2xl font-bold'>New Dashboard</span>
+                                                    <button className="btn" onClick={() => setIsModalOpen(false)}><i className="fa fa-times" aria-hidden="true"></i></button>
+                                                </div>
+                                                <div className='my-8'>
+                                                    <span className='text-left font-bold flex justify-start pl-10'>Storyboard Name</span>
+                                                    <input
+                                                        type="text"
+                                                        className='w-[400px] h-[45px] border mt-2 border-gray-200 form-control p-2 rounded'
+                                                        placeholder='e.g. Default Project Storyboard'
+                                                        value={inputValue}
+                                                        onChange={handleChange}
+                                                    />
+                                                    <span className='text-left font-bold flex justify-start pl-10 mt-3'>Frame Size</span>
+                                                    <select name="" id="" className='w-[400px] h-[45px] p-2 font-medium rounded border border-gray-200'>
+                                                        <option value="" className='p-2'>Landscape (16.9)</option>
+                                                        <option value="">Portrait</option>
+                                                    </select>
+                                                    <hr className='mt-10' />
+                                                    <div className='flex justify-between items-center my-5 p-3'>
+                                                        <div>
+                                                            <input
+                                                                className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                                                                type="checkbox"
+                                                                role="switch"
+                                                                id="flexSwitchCheckDefault"
+                                                                checked={isChecked}
+                                                                onChange={handleCheckboxChange}
+                                                            />
+                                                            <label
+                                                                className="inline-block pl-[0.15rem] hover:cursor-pointer font-bold"
+                                                                htmlFor="flexSwitchCheckDefault"
+                                                            >
+                                                                AI Assistant
+                                                            </label>
+                                                        </div>
+                                                        <div>
+                                                            <button
+                                                                className={`btn  text-white h-10 w-44 rounded ${!inputValue.trim() ? 'bg-slate-500 text-white' : 'bg-slate-800'}`}
+                                                                type='button'
+                                                                disabled={!inputValue.trim()}
+                                                            >
+                                                                Create dashboard
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </dialog>
+                            }
+                        </div>
+                    </div>
+                    <div className='h-auto max-w-screen bg-white rounded-xl'>
+                        <div className='flex text-center p-12 justify-between'>
+                            <div className='h-5 max-w-screen text-black'>
+                                <span className='text-xl font-bold'><i className="fa-regular fa-folder"></i></span>
+                                <span className='text-xl font-bold ml-2'>Default Project</span><br />
+                                <span className='text-slate-400'>1 Storyboard</span>
+                            </div>
+                            <div className='h-5'>
+                                <span><i className='fa fa-ellipsis-v'></i>&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                                <span><i className='fa fa-angle-down'></i> </span>
+                            </div>
+                        </div>
+                        <div className='p-5'>
+                            <div className='h-auto w-22 bg-white p-1'>
+                                <div className='grid grid-cols-6 md:grid-cols-12 ms-auto'>
+                                    <div className="max-w-4xl me-2 col-span-6 md:col-span-4 lg:col-span-4 bg-white border border-gray-50 rounded-lg shadow dark:bg-white white:border-gray-700">
+                                        <a href="#">
+                                            <img className="rounded-t-lg h-40 w-full" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712016000&semt=sph" alt="" />
+                                        </a>
+                                        <div className="p-5">
+                                            <a href="#">
+                                                <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">AI Character Demo Storyboard</h6>
+                                            </a>
+                                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Updated 19 mar 2023</p>
+                                        </div>
+                                    </div>
+                                    <div className="w-80  sm:w-96 h-72 my-2 lg:mx-2 sm:mx-0 bg-white border border-gray-100 rounded-lg shadow dark:bg-blue-50 white:border-gray-700 flex justify-center items-center">
+                                        <div className='h-14 w-14 bg-blue-50 border border-gray-300 rounded-full flex justify-center items-center'>
+                                        <span className='text-gray-300' onClick={() => setIsModalOpen1(true)} ><i className="fa fa-plus "></i></span>
+                                            {/* <button className="btn bg-slate-900 text-white rounded h-12 w-36" onClick={() => setIsModalOpen1(true)}></button> */}
+                                            {isModalOpen1 &&
+                                                <dialog className={`modal ${isModalOpen1 ? 'w-full md:w-2/6' : 'hidden'} bg-white shadow-lg rounded-xl`} open>
+                                                    <div className="modal-box">
+                                                        <div className="modal-action p-5">
+                                                            <form method="dialog">
+                                                                <div className='flex justify-between'>
+                                                                    <span className='text-2xl font-bold'>New Dashboard</span>
+                                                                    <button className="btn" onClick={() => setIsModalOpen1(false)}><i className="fa fa-times" aria-hidden="true"></i></button>
+                                                                </div>
+                                                                <div className='my-8'>
+                                                                    <span className='text-left font-bold flex justify-start pl-10'>Storyboard Name</span>
+                                                                    <input
+                                                                        type="text"
+                                                                        className='w-[400px] h-[45px] border mt-2 border-gray-200 form-control p-2 rounded'
+                                                                        placeholder='e.g. Default Project Storyboard'
+                                                                        value={inputValue}
+                                                                        onChange={handleChange}
+                                                                    />
+                                                                    <span className='text-left font-bold flex justify-start pl-10 mt-3'>Frame Size</span>
+                                                                    <select name="" id="" className='w-[400px] h-[45px] p-2 font-medium rounded border border-gray-200'>
+                                                                        <option value="" className='p-2'>Landscape (16.9)</option>
+                                                                        <option value="">Portrait</option>
+                                                                    </select>
+                                                                    <hr className='mt-10' />
+                                                                    <div className='flex justify-between items-center my-5 p-3'>
+                                                                        <div>
+                                                                            <input
+                                                                                className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                                                                                type="checkbox"
+                                                                                role="switch"
+                                                                                id="flexSwitchCheckDefault"
+                                                                                checked={isChecked}
+                                                                                onChange={handleCheckboxChange}
+                                                                            />
+                                                                            <label
+                                                                                className="inline-block pl-[0.15rem] hover:cursor-pointer font-bold"
+                                                                                htmlFor="flexSwitchCheckDefault"
+                                                                            >
+                                                                                AI Assistant
+                                                                            </label>
+                                                                        </div>
+                                                                        <div>
+                                                                            <button
+                                                                                className={`btn  text-white h-10 w-44 rounded ${!inputValue.trim() ? 'bg-slate-500 text-white' : 'bg-slate-800'}`}
+                                                                                type='button'
+                                                                                disabled={!inputValue.trim()}
+                                                                            >
+                                                                                Create dashboard
+                                                                            </button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </dialog>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Card;
---
+++ src/components/Mynavbar.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+
+
+const Mynavbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen1, setIsOpen1] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const toggleMenu1 = () => {
+        setIsOpen1(!isOpen1);
+    };
+
+    return (
+        <nav className="bg-white p-2">
+            <div className="mx-auto max-w-7xl  ">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <button
+                            type="button"
+                            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            aria-controls="mobile-menu"
+                            aria-expanded={isOpen1 ? 'true' : 'false'}
+                            onClick={toggleMenu1}
+                        >
+                            <span className="absolute -inset-0.5"></span>
+                            <span className="sr-only">Open main menu</span>
+
+                            <svg
+                                className={isOpen1 ? 'hidden h-6 w-6' : 'block h-6 w-6'}
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+
+                            <svg
+                                className={isOpen1 ? 'block h-6 w-6' : 'hidden h-6 w-6'}
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="hidden sm:flex  items-center">
+                            <img
+                                className="h-8 w-auto rounded-full "
+                                src="https://static.vecteezy.com/system/resources/previews/007/697/285/original/black-and-white-b-modern-b-logo-capital-b-logo-free-vector.jpg"
+                                alt="Your Company"
+                            />
+                        </div>
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                <a href="#" className=" text-black text-2xl rounded-md px-3 py-2 font-bold" aria-current="page">
+                                    Project Dashboard
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 19l-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input
+                                type="search"
+                                id="default-search"
+                                className="block w-full sm:w-80 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search My Storyboards"
+                                required
+                            />
+                        </div>
+                        <div className='ms-5 hidden md:block '>
+                            <button className='btn bg-blue-100 h-12 w-44 rounded-xl  '>
+                                <span className='font-bold'>Quick Start</span>
+                            </button>
+                        </div>
+                        <div className="relative ml-3">
+                            <div>
+                                <button
+                                    type="button"
+                                    className="relative flex rounded-full  bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    id="user-menu-button"
+                                    aria-expanded={isOpen ? 'true' : 'false'}
+                                    onClick={toggleMenu}
+                                >
+                                    <span className="absolute -inset-1.5"></span>
+                                    <span className="sr-only">Open user menu</span>
+                                    <div className='h-12 w-12 rounded-full text-white bg-gradient-to-r to-yellow-500 from-slate-100  flex justify-center items-center '>
+                                        <span className='text-black font-bold  text-2xl ' > A </span>
+                                    </div>
+                                </button>
+                            </div>
+
+                            {isOpen && (
+                                <div
+                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu"
+                                    aria-orientation="vertical"
+                                    aria-labelledby="user-menu-button"
+                                    tabIndex="-1"
+                                >
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">
+                                        Your Profile
+                                    </a>
+
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={isOpen1 ? 'block sm:hidden' : 'hidden'} id="mobile-menu">
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                    <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">
+                        Dashboard
+                    </a>
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                        Team
+                    </a>
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                        Projects
+                    </a>
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                        Calendar
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Mynavbar;
